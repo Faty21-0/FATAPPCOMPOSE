9ba0b069057cf47c3a31146088256f39f1dcb78e
Fatima Rivasplata <22200268@ue.edu.pe>
2026-04-25 12:35:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fatappcompose/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.fatappcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fatappcompose.ui.theme.FATAPPCOMPOSETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloComposeForm()
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelloComposeForm(){
+    var name by remember{ mutableStateOf( "") }
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {Text("Hola ESAN")})
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues = padding)
+                .padding(all = 16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+            ){
+            Text("BIENVENIDO A FATCOMPOSE")
+            OutlinedTextField(
+                value = name,
+                onValueChange = {name = it},
+                label ={Text("Nombre")}
+            )
+            Button(
+                onClick ={},
+                enabled = name.isNotEmpty()
+            ){
+                Text("Enviar")
+            }
+
+        }
+
+
+    }
+
+}
